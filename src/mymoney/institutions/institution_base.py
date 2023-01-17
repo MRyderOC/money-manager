17501d270ba6931f7dcbf33b86c02d30ed50839b
Milad <63479762+MRyderOC@users.noreply.github.com>
2023-01-17 01:21:11 -0600

    Service class added for Institution

--- src/mymoney/institutions/institution_base.py
+++ src/mymoney/institutions/institution_base.py
@@ -1,19 +1,19 @@
 import json
 import logging
 import dataclasses
+from enum import Enum
 from typing import List, Dict, Union
 
 import numpy as np
 import pandas as pd
 from importlib_resources import files
 
-from mymoney.core.data_validation import DataFrameValidation
+from mymoney.utils.data_validation import DataFrameValidation
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(name)s\t[%(asctime)s] %(levelname)s: %(message)s",
     datefmt="%b/%d/%y %I:%M:%S %p",
-    # filename="logs.log",
 )
 
 
@@ -23,6 +23,68 @@ class TransformedData:
     sanity_df: pd.DataFrame
     output_df: pd.DataFrame
     out_type: str
+
+
+class ServiceType(Enum):
+    BASE = "base"
+    CREDIT = "credit"
+    DEBIT = "debit"
+    THIRDPARTY = "3rdparty"
+    EXCHANGE = "exchange"
+
+
+class Service():
+    """docs here!"""
+
+    _service_type = ServiceType("base")
+
+    def __init__(self, inst_meta_data: Dict) -> None:
+        md = inst_meta_data.get(self._service_type.value)
+        if not md:
+            raise Exception(
+                f"No `{self._service_type.value}` service available in inst_meta_data."
+            )
+        self._this_meta_data = md
+
+
+    def _data_validation(self, input_df: pd.DataFrame) -> pd.DataFrame:
+        """docs here!"""
+        val_data = self._this_meta_data.get("validation_data")
+        if not val_data:
+            raise Exception("Validation data is not available.")
+
+        schema = val_data.get("schema")
+        col_vals = val_data.get("column_values")
+
+        df_validate_instance = DataFrameValidation(input_df)
+        if schema:
+            df_validate_instance.has_schema(schema)
+        if col_vals:
+            input_df["_new_IsValid"] = df_validate_instance.has_vals(
+                col_vals_dict=col_vals, return_validation_col=True
+            )
+
+        return input_df
+
+
+    def _cleaning(self, input_df: pd.DataFrame, account_name: str) -> pd.DataFrame:
+        """...
+
+        Args:
+            input_df (pd.DataFrame):
+                ...
+            account_name (str):
+                ...
+
+        Returns:
+            ...
+        """
+        ...
+
+
+    def create_sanity_data(self, input_df: pd.DataFrame, account_name: str):
+        """docs here!"""
+        return self._data_validation(self._cleaning(input_df, account_name))
 
 
 class Institution():
@@ -48,6 +110,27 @@ class Institution():
     ]
 
 
+    class CreditService(Service):
+        """Prototype class for Credit Service."""
+        _service_type = ServiceType("credit")
+        ...
+
+    class DebitService(Service):
+        """Prototype class for Debit Service."""
+        _service_type = ServiceType("debit")
+        ...
+
+    class ThirdPartyService(Service):
+        """Prototype class for ThirdParty Service."""
+        _service_type = ServiceType("3rdparty")
+        ...
+
+    class ExchangeService(Service):
+        """Prototype class for Exchange Service."""
+        _service_type = ServiceType("exchange")
+        ...
+
+
     def __init__(self) -> None:
         self._meta_data = json.loads(
             files("mymoney").joinpath("meta_data.json").read_text()
@@ -71,130 +154,35 @@ class Institution():
         return df.drop(columns=old_columns).rename(columns=new_columns_name_map)
 
 
-    def _data_validation(
-        self, input_df: pd.DataFrame, service_name: str
-    ) -> pd.DataFrame:
-        """docs here!"""
-        val_data = self._this_meta_data[service_name].get("validation_data")
-        if not val_data:
-            raise Exception(
-                "Validation data is not available."
-            )
-
-        schema = val_data.get("schema")
-        col_vals = val_data.get("column_values")
-
-        df_validate_instance = DataFrameValidation(input_df)
-        if schema:
-            df_validate_instance.has_schema(schema)
-        if col_vals:
-            input_df["_new_IsValid"] = df_validate_instance.has_vals(
-                col_vals_dict=col_vals, return_validation_col=True
-            )
-
-        return input_df
-
-
     def service_executer(
         self,
         input_df: pd.DataFrame,
         service_name: str,
         account_name: str
     ) -> TransformedData:
+        """docs here!"""
         if service_name == "debit":
-            transformed_data = self.debit(input_df, account_name)
+            the_service = self.DebitService(self._this_meta_data)
+            out_type = "balance"
         elif service_name == "credit":
-            transformed_data = self.credit(input_df, account_name)
+            the_service = self.CreditService(self._this_meta_data)
+            out_type = "expense"
         elif service_name == "3rdparty":
-            transformed_data = self.third_party(input_df, account_name)
+            the_service = self.ThirdPartyService(self._this_meta_data)
+            out_type = "expense"
         elif service_name == "exchange":
-            transformed_data = self.exchange(input_df, account_name)
+            the_service = self.ExchangeService(self._this_meta_data)
+            out_type = "trade"
         else:
             raise ValueError(
                 "service_name should be one of the following:"
                 " 'debit', 'credit', '3rdparty', 'exchange'."
             )
 
-        return transformed_data
-
-
-    def _credit_cleaning(
-        self, input_df: pd.DataFrame, account_name: str
-    ) -> pd.DataFrame:
-        """Prototype function that each subclass of Institution should implement if they have `credit` services."""
-        ...
-
-    def _debit_cleaning(
-        self, input_df: pd.DataFrame, account_name: str
-    ) -> pd.DataFrame:
-        """Prototype function that each subclass of Institution should implement if they have `debit` services."""
-        ...
-
-    def _third_party_cleaning(
-        self, input_df: pd.DataFrame, account_name: str
-    ) -> pd.DataFrame:
-        """Prototype function that each subclass of Institution should implement if they have `3rdparty` services."""
-        ...
-
-    def _exchange_cleaning(
-        self, input_df: pd.DataFrame, account_name: str
-    ) -> pd.DataFrame:
-        """Prototype function that each subclass of Institution should implement if they have `exchange` services."""
-        ...
-
-
-    def debit(
-        self, input_df: pd.DataFrame, account_name: str
-    ) -> TransformedData:
-        """docs here!"""
-        whole_df = self._debit_cleaning(input_df, account_name)
-        sanity_df = self._data_validation(whole_df, "debit")
+        sanity_df = the_service.create_sanity_data(input_df, account_name)
         out_df = self._output_df_creator(sanity_df)
-
         return TransformedData(
             sanity_df=sanity_df,
             output_df=out_df,
-            out_type="balance",
-        )
-
-    def credit(
-        self, input_df: pd.DataFrame, account_name: str
-    ) -> TransformedData:
-        """docs here!"""
-        whole_df = self._credit_cleaning(input_df, account_name)
-        sanity_df = self._data_validation(whole_df, "credit")
-        out_df = self._output_df_creator(sanity_df)
-
-        return TransformedData(
-            sanity_df=sanity_df,
-            output_df=out_df,
-            out_type="expense",
-        )
-
-    def third_party(
-        self, input_df: pd.DataFrame, account_name: str
-    ) -> TransformedData:
-        """docs here!"""
-        whole_df = self._third_party_cleaning(input_df, account_name)
-        sanity_df = self._data_validation(whole_df, "3rdparty")
-        out_df = self._output_df_creator(sanity_df)
-
-        return TransformedData(
-            sanity_df=sanity_df,
-            output_df=out_df,
-            out_type="expense",
-        )
-
-    def exchange(
-        self, input_df: pd.DataFrame, account_name: str
-    ) -> TransformedData:
-        """docs here!"""
-        whole_df = self._exchange_cleaning(input_df, account_name)
-        sanity_df = self._data_validation(whole_df, "exchange")
-        out_df = self._output_df_creator(sanity_df)
-
-        return TransformedData(
-            sanity_df=sanity_df,
-            output_df=out_df,
-            out_type="trade",
+            out_type=out_type,
         )
