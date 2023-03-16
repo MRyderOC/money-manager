220db45b77d51f4bf83473371e49f6f7bae33cc4
Milad <63479762+MRyderOC@users.noreply.github.com>
2023-03-15 20:38:46 -0500

    conf.py for docs updated

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ release = '0.0.2'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc', 'nbsphinx']
+extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
