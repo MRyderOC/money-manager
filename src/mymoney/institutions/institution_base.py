211dc5e7fb21cc47d2aa50b95c30347297117062
Milad <63479762+MRyderOC@users.noreply.github.com>
2023-04-24 00:16:10 -0500

    base class for institutions and it's related components updated

--- src/mymoney/institutions/institution_base.py
+++ src/mymoney/institutions/institution_base.py
@@ -1,14 +1,13 @@
 import json
 import logging
-import dataclasses
 from enum import Enum
-from typing import List, Dict, Union
+from typing import Dict
 
-import numpy as np
 import pandas as pd
 from importlib_resources import files
 
 from mymoney.utils.data_validation import DataFrameValidation
+
 
 logging.basicConfig(
     level=logging.INFO,
@@ -17,15 +16,15 @@ logging.basicConfig(
 )
 
 
-@dataclasses.dataclass
-class TransformedData:
-    """docs here!"""
-    sanity_df: pd.DataFrame
-    output_df: pd.DataFrame
-    out_type: str
+class DataType(Enum):
+    """Enum class for compatible data types to process."""
+    CSV = "csv"
+    PDF = "pdf"
+    ANY = "any"
 
 
 class ServiceType(Enum):
+    """Enum class for different service types for each institution."""
     BASE = "base"
     CREDIT = "credit"
     DEBIT = "debit"
@@ -33,10 +32,10 @@ class ServiceType(Enum):
     EXCHANGE = "exchange"
 
 
-class Service():
-    """docs here!"""
+class Service:
+    """Base class for each service of an institution."""
 
-    _service_type = ServiceType("base")
+    _service_type = ServiceType.BASE
 
     def __init__(self, inst_meta_data: Dict) -> None:
         md = inst_meta_data.get(self._service_type.value)
@@ -47,8 +46,17 @@ class Service():
         self._this_meta_data = md
 
 
-    def _data_validation(self, input_df: pd.DataFrame) -> pd.DataFrame:
-        """docs here!"""
+    def _data_validation(self, df: pd.DataFrame) -> pd.DataFrame:
+        """Creates the `IsValid` column for the `df` based on the data
+        available in meta_data for this specific service.
+
+        Args:
+            df (pd.DataFrame):
+                The input DataFrame.
+
+        Returns:
+            The same DataFrame with a new column `_new_IsValid`.
+        """
         val_data = self._this_meta_data.get("validation_data")
         if not val_data:
             raise Exception("Validation data is not available.")
@@ -56,59 +64,68 @@ class Service():
         schema = val_data.get("schema")
         col_vals = val_data.get("column_values")
 
-        df_validate_instance = DataFrameValidation(input_df)
+        df_validate_instance = DataFrameValidation(df)
         if schema:
             df_validate_instance.has_schema(schema)
         if col_vals:
-            input_df["_new_IsValid"] = df_validate_instance.has_vals(
+            df["_new_IsValid"] = df_validate_instance.has_vals(
                 col_vals_dict=col_vals, return_validation_col=True
             )
 
-        return input_df
+        return df
 
 
-    def _cleaning(self, input_df: pd.DataFrame, account_name: str) -> pd.DataFrame:
-        """...
+    def _csv_cleaning(
+            self, input_df: pd.DataFrame, account_name: str
+    ) -> pd.DataFrame:
+        """Prototype class for cleaning process of CSV files for this service.
+        Subclasses should implement this method.
 
         Args:
             input_df (pd.DataFrame):
-                ...
+                The input DataFrame.
             account_name (str):
-                ...
+                The name of the account associated with this service.
 
         Returns:
-            ...
+            The same DataFrame with new columns for cleaned data.
+
+        Raises:
+            NotImplementedError: If the subclass hasn't implemented this method.
         """
-        ...
+        raise NotImplementedError("Subclass should implement this method!")
 
 
-    def create_sanity_data(self, input_df: pd.DataFrame, account_name: str) -> pd.DataFrame:
-        """docs here!"""
-        return self._data_validation(self._cleaning(input_df, account_name))
+    def data_type_exec(
+            self, data_type: DataType, table: pd.DataFrame, account_name: str
+    ) -> pd.DataFrame:
+        """Execute the cleaning function corresponded to `data_type`
+        and validate the results.
+
+        Args:
+            data_type (DataType):
+                The type of input data. Refer to `DataType` for supported types.
+            table (pd.DataFrame):
+                The input table as a DataFrame.
+            account_name (str):
+                The name of the account associated with this service.
+
+        Returns:
+            The same DataFrame with new columns for cleaned data.
+        """
+        match data_type:
+            case DataType.CSV: df = self._csv_cleaning(input_df=table, account_name=account_name)
+            case DataType.PDF: raise NotImplementedError("Not supported yet!")
+            case DataType.ANY: raise NotImplementedError("Not supported yet!")
+
+        return self._data_validation(df)
 
 
-class Institution():
-    """docs here!"""
+class Institution:
+    """Base class for institutions to do data transformation related operations."""
 
     _this_institution_name = "base"
-
     _USDs = ["USD", "USDC", "USDT"]
-    _new_expense_columns = [
-        "Description", "Amount", "Date", "Institution",
-        "InstitutionCategory", "MyCategory",
-        "IsTransfer", "IsValid",
-    ]
-    _new_trade_columns = [
-        "Datetime",
-        "From Account", "To Account",
-        "From Asset", "To Asset",
-        "In Amount", "Out Amount",
-        "Fee Asset", "Fee Amount", "Fee Value",
-        "Trx Type", "Trx Sub Type",
-        "Asset Type",
-        "USD Amount",
-    ]
-
 
     def __init__(self) -> None:
         self._meta_data = json.loads(
@@ -117,72 +134,57 @@ class Institution():
         self._this_meta_data = self._meta_data.get(self._this_institution_name)
 
 
-    def _output_df_creator(self, df: pd.DataFrame) -> pd.DataFrame:
-        """docs here!"""
-        new_columns_name_map = {
-            col: col[5:]
-            for col in df.columns
-            if col.startswith("_new_")
-        }
-        old_columns = [
-            col
-            for col in df.columns
-            if not col.startswith("_new_")
-        ]
-
-        return df.drop(columns=old_columns).rename(columns=new_columns_name_map)
-
-
     def service_executer(
         self,
-        input_df: pd.DataFrame,
         service_name: str,
+        data_type: DataType,
+        table: pd.DataFrame,
         account_name: str
-    ) -> TransformedData:
-        """docs here!"""
-        if service_name == "debit":
-            the_service = self.DebitService(self._this_meta_data)
-            out_type = "balance"
-        elif service_name == "credit":
-            the_service = self.CreditService(self._this_meta_data)
-            out_type = "expense"
-        elif service_name == "3rdparty":
-            the_service = self.ThirdPartyService(self._this_meta_data)
-            out_type = "expense"
-        elif service_name == "exchange":
-            the_service = self.ExchangeService(self._this_meta_data)
-            out_type = "trade"
-        else:
-            raise ValueError(
-                "service_name should be one of the following:"
-                " 'debit', 'credit', '3rdparty', 'exchange'."
-            )
+    ) -> pd.DataFrame:
+        """Execute the cleaning function corresponded to `service_name`.
 
-        sanity_df = the_service.create_sanity_data(input_df, account_name)
-        out_df = self._output_df_creator(sanity_df)
-        return TransformedData(
-            sanity_df=sanity_df,
-            output_df=out_df,
-            out_type=out_type,
+        Args:
+            service_name (str):
+                The service of the input data. Refer to `ServiceType` for supported services.
+            data_type (DataType):
+                The type of the input data. Refer to `DataType` for supported types.
+            table (pd.DataFrame):
+                The input table as a DataFrame.
+            account_name (str):
+                The name of the account associated with this service.
+
+        Returns:
+            The same DataFrame with new columns for cleaned data.
+        """
+        md = self._this_meta_data
+        match service_name:
+            case "debit": the_serv = self.DebitService(md)
+            case "credit": the_serv = self.CreditService(md)
+            case "3rdparty": the_serv = self.ThirdPartyService(md)
+            case "exchange": the_serv = self.ExchangeService(md)
+            case _:
+                raise ValueError(
+                    "service_name should be one of the following:"
+                    " ['debit', 'credit', '3rdparty', 'exchange']."
+                )
+
+        return the_serv.data_type_exec(
+            data_type=data_type, table=table, account_name=account_name
         )
 
 
     class CreditService(Service):
         """Prototype class for Credit Service."""
-        _service_type = ServiceType("credit")
-        ...
+        _service_type = ServiceType.CREDIT
 
     class DebitService(Service):
         """Prototype class for Debit Service."""
-        _service_type = ServiceType("debit")
-        ...
+        _service_type = ServiceType.DEBIT
 
     class ThirdPartyService(Service):
         """Prototype class for ThirdParty Service."""
-        _service_type = ServiceType("3rdparty")
-        ...
+        _service_type = ServiceType.THIRDPARTY
 
     class ExchangeService(Service):
         """Prototype class for Exchange Service."""
-        _service_type = ServiceType("exchange")
-        ...
+        _service_type = ServiceType.EXCHANGE
