9bea6b8ea81eabd3a7be406e6853e1174ced3d99
Milad <63479762+MRyderOC@users.noreply.github.com>
2023-08-21 23:51:25 -0500

    DebitService for CapitalOne added

--- src/mymoney/institutions/capitalone.py
+++ src/mymoney/institutions/capitalone.py
@@ -79,3 +79,49 @@ class CapitalOne(institution_base.Institution):
             input_df["_new_IsTransfer"] = input_df.apply(is_transfer_finder, axis=1)  # noqa: E501
 
             return input_df
+
+    class DebitService(institution_base.Institution.DebitService):
+        """A class for Credit Service."""
+
+        def _csv_cleaning(
+            self, input_df: pd.DataFrame, account_name: str
+        ) -> pd.DataFrame:
+            """A method for cleaning process of CSV files for this service.
+
+            Args:
+                input_df (pd.DataFrame):
+                    The input DataFrame.
+                account_name (str):
+                    The name of the account associated with this service.
+
+            Returns:
+                The same DataFrame with new columns for cleaned data.
+            """
+
+            def is_transfer_finder(row):
+                try:
+                    regex_flag = re.search(
+                        r"VENMO|DISCOVER|AMEX|WELLS FARGO|PAYPAL|CITI CARD|"
+                        r"CAPITAL ONE [\w\s]*PMT|SOFI [\w\s\.]* CARD PAYMT|"
+                        r"360 Checking",
+                        str(row["_new_Description"])
+                    )
+                except Exception:
+                    return "consider"
+
+                if regex_flag:
+                    return "transfer"
+                else:
+                    return "expense"
+
+            input_df["_new_Description"] = input_df["Transaction Description"].map(lambda val: str(val).strip())  # noqa: E501
+            input_df["_new_Amount"] = input_df["Transaction Amount"].copy(deep=True)  # noqa: E501
+            input_df["_new_Date"] = input_df["Transaction Date"].copy(deep=True)  # noqa: E501
+            input_df["_new_InstitutionCategory"] = np.nan
+            input_df["_new_MyCategory"] = np.nan
+            input_df["_new_Institution"] = "Capital One"
+            input_df["_new_AccountName"] = input_df["Account Number"].copy(deep=True)  # noqa: E501
+            input_df["_new_Service"] = self._service_type.value
+            input_df["_new_IsTransfer"] = input_df.apply(is_transfer_finder, axis=1)  # noqa: E501
+
+            return input_df
