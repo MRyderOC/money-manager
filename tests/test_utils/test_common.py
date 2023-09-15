a8aaa4e1df3b6b3e6d920d9c4c2a27997837a7f6
Milad <63479762+MRyderOC@users.noreply.github.com>
2023-09-14 23:41:43 -0500

    tests added for raise_or_log

--- tests/test_utils/test_common.py
+++ tests/test_utils/test_common.py
@@ -1,6 +1,7 @@
 import pytest
 
 from mymoney.utils.common import column_name_checker
+from mymoney.utils.common import raise_or_log
 from mymoney.utils.exceptions import DifferentColumnNameException
 
 
@@ -26,3 +27,30 @@ def test_column_name_checker_superset(
     column_name_checker(dataframe_creator, subset_column, "superset")
     with pytest.raises(DifferentColumnNameException):
         column_name_checker(dataframe_creator, superset_column, "superset")
+
+
+def test_raise_or_log_with_Exception():
+    with pytest.raises(Exception) as exception_info:
+        msg = "Exception message!"
+        raise_or_log(
+            message=msg, logs=False, raises=True, exception_type=Exception
+        )
+    assert "Exception message!" in str(exception_info.value)
+
+
+def test_raise_or_log_with_ValueError():
+    with pytest.raises(ValueError) as exception_info:
+        msg = "ValErr message!"
+        raise_or_log(
+            message=msg, logs=False, raises=True, exception_type=ValueError
+        )
+    assert "ValErr message!" in str(exception_info.value)
+
+
+def test_raise_or_log_with_OSError():
+    with pytest.raises(OSError) as exception_info:
+        msg = "OSError message!"
+        raise_or_log(
+            message=msg, logs=False, raises=True, exception_type=OSError
+        )
+    assert "OSError message!" in str(exception_info.value)
