72205164015da17f5e80e6032f71615ae903945e
Milad <63479762+MRyderOC@users.noreply.github.com>
2023-09-25 23:35:06 -0500

    test_is_shape added

--- tests/test_utils/test_data_validation.py
+++ tests/test_utils/test_data_validation.py
@@ -13,3 +13,9 @@ def test_has_dtype(range_series):
 def test_has_no_x(range_series):
     assert range_series.validate._check_no_x([1, 2, "a"]) is True
     assert range_series.validate._check_no_x([3, 4]) == [0, 1]
+
+
+def test_is_shape(dataframe_creator):
+    df_val = DataFrameValidation(dataframe_creator)
+    with pytest.raises(Exception):
+        df_val.is_shape((3, 4), raises=True)
