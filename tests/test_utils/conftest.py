d98e251954adb8c0acb028c7890fe7e28feb2765
Milad <63479762+MRyderOC@users.noreply.github.com>
2023-09-27 20:13:35 -0500

    more tests for data_validation

--- tests/test_utils/conftest.py
+++ tests/test_utils/conftest.py
@@ -28,5 +28,35 @@ def equal_column():
 
 
 @pytest.fixture
-def range_series():
+def series_int():
     return pd.Series(np.arange(3, 13))
+
+
+@pytest.fixture
+def series_string():
+    return pd.Series(["a" * i for i in range(10)])
+
+
+@pytest.fixture
+def series_bool():
+    return pd.Series([True, True, False, True, False] * 2)
+
+
+@pytest.fixture
+def series_float():
+    return pd.Series(np.arange(.1, 1, .1))
+
+
+@pytest.fixture
+def series_complex():
+    return pd.Series([complex(1, i) for i in range(10)])
+
+
+@pytest.fixture
+def series_datetime():
+    return pd.Series(pd.date_range("2020-01-01", periods=10, freq="M"))
+
+
+@pytest.fixture
+def series_timedelta():
+    return pd.Series([pd.Timedelta(days=i, hours=i) for i in range(10)])
--- tests/test_utils/test_data_validation.py
+++ tests/test_utils/test_data_validation.py
@@ -4,10 +4,25 @@ from mymoney.utils.data_validation import SeriesValidation  # noqa: F401
 from mymoney.utils.data_validation import DataFrameValidation  # noqa: F401
 
 
-def test_has_dtype(range_series):
-    assert range_series.validate._check_dtype("int") is True
+def test_has_dtype(
+    series_bool, series_string,
+    series_int, series_float, series_complex,
+    series_datetime, series_timedelta,
+):
+    to_test_tuples = [
+        (series_bool, "bool"),
+        (series_string, "string"),
+        (series_int, "int"),
+        (series_float, "float"),
+        (series_complex, "complex"),
+        (series_datetime, "datetime"),
+        (series_timedelta, "timedelta64"),
+    ]
+    for ser, dtype in to_test_tuples:
+        assert ser.validate._check_dtype(dtype) is True
+
     with pytest.raises(Exception):
-        range_series.validate.has_dtype("datetime", raises=True)
+        series_int.validate.has_dtype("datetime", raises=True)
 
 
 def test_has_no_x(range_series):
