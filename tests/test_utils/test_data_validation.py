8cdcb26eaf5e881937d8c8a749bce4e2f481bed3
Milad <63479762+MRyderOC@users.noreply.github.com>
2023-10-29 23:36:26 -0500

    updated tests

--- tests/test_utils/test_data_validation.py
+++ tests/test_utils/test_data_validation.py
@@ -38,7 +38,7 @@ def test_is_shape(dataframe_creator):
 
 def test_has_dtypes(dataframe_with_different_types):
     df_val = DataFrameValidation(dataframe_with_different_types)
-    df_val.has_dtypes({
+    dtpes_map_correct = {
         "series_int": "int",
         "series_string": "string",
         "series_bool": "bool",
@@ -46,4 +46,17 @@ def test_has_dtypes(dataframe_with_different_types):
         "series_complex": "complex",
         "series_datetime": "datetime",
         "series_timedelta": "timedelta64",
-    })
+    }
+    df_val.has_dtypes(dtpes_map_correct)
+
+    dtpes_map_false = {
+        "series_int": "float",
+        "series_string": "string",
+        "series_bool": "bool",
+        "series_float": "int",
+        "series_complex": "complex",
+        "series_datetime": "datetime",
+        "series_timedelta": "timedelta64",
+    }
+    with pytest.raises(Exception):
+        df_val.has_dtypes(dtpes_map_false, raises=True)
