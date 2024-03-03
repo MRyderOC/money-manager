cadaeff063f8598572c2fb8428ded47b9bcded91
MRyderOC <milad.tabrizi@gmail.com>
2024-03-03 15:58:07 -0600

    Update institutions __init__

--- src/mymoney/institutions/__init__.py
+++ src/mymoney/institutions/__init__.py
@@ -10,8 +10,12 @@ from mymoney.institutions.capitalone import CapitalOne
 from mymoney.institutions.cashapp import CashApp
 from mymoney.institutions.chase import Chase
 from mymoney.institutions.citi import Citi
+from mymoney.institutions.coinbase import Coinbase
+from mymoney.institutions.cryptodotcom import CryptoDotCom
 from mymoney.institutions.discover import Discover
 from mymoney.institutions.paypal import PayPal
+from mymoney.institutions.samsclub import SamsClub
+from mymoney.institutions.uphold import Uphold
 from mymoney.institutions.venmo import Venmo
 from mymoney.institutions.wellsfargo import WellsFargo
 
@@ -27,8 +31,12 @@ __all__ = [
     "CashApp",
     "Chase",
     "Citi",
+    "Coinbase",
+    "CryptoDotCom",
     "Discover",
     "PayPal",
+    "SamsClub",
+    "Uphold",
     "Venmo",
     "WellsFargo",
 ]
