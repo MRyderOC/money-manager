d5eef2f0f30dba7bf5af6826c858687ea6ed7a46
MRyderOC <milad.tabrizi@gmail.com>
2024-03-11 13:47:50 -0500

    Update core __init__

--- src/mymoney/core/__init__.py
+++ src/mymoney/core/__init__.py
@@ -1,7 +1,9 @@
 from mymoney.core.data_reader import DataReader
 from mymoney.core.data_classes import InstData
+from mymoney.core.my_data import MyData
 
 __all__ = [
     "DataReader",
     "InstData",
+    "MyData",
 ]
